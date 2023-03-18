87428e8bbcec7a864492a77b69bf47d010433857
Reshma  Ramachandran <r.reshma67@gmail.com>
2023-03-18 12:22:24 -0700

    Footer with Navigation bar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +29,6 @@ namespace UWPPhotoLibrary
         {
             this.InitializeComponent();
         }
+        
     }
 }
